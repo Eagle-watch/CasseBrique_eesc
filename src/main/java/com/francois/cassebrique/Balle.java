ee82764d65c44ffd21f3fa6c13d8170b5ef4d582
54bryan.francois <b.francois.gj@gmail.com>
2022-09-26 11:19:02 +0200

    Casse Brique Classe Balle rajout

---
+++ src/main/java/com/francois/cassebrique/Balle.java
@@ -0,0 +1,82 @@
+package com.francois.cassebrique;
+
+import java.awt.*;
+
+public class Balle {
+
+    protected  int x;
+    protected  int y;
+    protected  int vitesseHorizontale;
+    protected  int vitesseVerticale;
+    protected  int diametre;
+    protected  int diametre2;
+    protected  int decalle2;
+    protected Color couleur;
+
+    public Balle(int x, int y, int vitesseHorizontale, int vitesseVerticale, int diametre, Color couleur) {
+        this.x = x;
+        this.y = y;
+        this.vitesseHorizontale = vitesseHorizontale;
+        this.vitesseVerticale = vitesseVerticale;
+        this.couleur = couleur;
+        this.setDiametre(diametre);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getVitesseHorizontale() {
+        return vitesseHorizontale;
+    }
+
+    public void setVitesseHorizontale(int vitesseHorizontale) {
+        this.vitesseHorizontale = vitesseHorizontale;
+    }
+
+    public int getVitesseVerticale() {
+        return vitesseVerticale;
+    }
+
+    public void setVitesseVerticale(int vitesseVerticale) {
+        this.vitesseVerticale = vitesseVerticale;
+    }
+
+    public int getDiametre() {
+        return diametre;
+    }
+
+    public void setDiametre(int diametre) {
+        this.diametre = diametre;
+        this.diametre2 = diametre * 20 / 100;
+        this.decalle2 = diametre * 20 / 100;
+    }
+
+    public int getDiametre2() {
+        return diametre2;
+    }
+
+    public int getDecalle2() {
+        return decalle2;
+    }
+
+    public Color getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(Color couleur) {
+        this.couleur = couleur;
+    }
+}
